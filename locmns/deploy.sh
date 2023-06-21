193c8205a44acafa2248183003a64f410c9a7bce
walid <hamoudwalid57@gmail.com>
2023-06-21 11:16:36 +0200

    commit avant mise en prod

--- locmns/deploy.sh
+++ locmns/deploy.sh
@@ -2,7 +2,7 @@
 # Mettre à jour le code source
 git pull
 # Construire le projet avec Maven
-bash mvnw package -P prod -e
+bash mvnw package -P prod,sysadmin --settings /home/debian/.m2/settings.xml
 # Construire l'image Docker
 docker build --no-cache -t image-spring-locmns .
 # Arreter le conteneur existant
